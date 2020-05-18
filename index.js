b3be21577bf0a5e5b011bde6f999607e3baad993
Ayush Arya <arya.ayush409@gmail.com>
2020-05-18 01:27:20 -0400

    global bodyparser middleware added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	res.send('Hi there');
+	res.send(`
+    <form method="POST">
+        <input name="email" placeholder="Email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="passwordconfirmation" />
+        <button>Sign Up</button>
+    </form>
+    `);
+});
+
+app.post('/', (re, res) => {
+	res.send('Done');
 });
 
 app.listen(3000, () => {
