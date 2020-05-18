172da128f3aad011e1961c758b50af618b8369ce
Ayush Arya <arya.ayush409@gmail.com>
2020-05-18 17:39:35 -0400

    create and save functionality added in user class

--- repositories/users.js
+++ repositories/users.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 class UserRepository {
 	constructor(filename) {
-		// require synchronus function
+		// constructor require synchronus function
 		if (!filename) {
 			throw new Error('Creating a repository require a File name');
 		}
@@ -10,9 +10,31 @@ class UserRepository {
 		try {
 			fs.accessSync(this.filename);
 		} catch (err) {
-			fs.writeFileSync(this.filename, []);
+			fs.writeFileSync(this.filename, '[]');
 		}
 	}
-}
 
-const repo = new UserRepository('users.json');
+	async getall() {
+		//open file, read content, parse json content, return this data
+		return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+	}
+
+	async create(attrib) {
+		//{email:"", pass:""}
+		const records = await this.getall();
+		records.push(attrib);
+		await this.writeall(records);
+	}
+
+	async writeall(records) {
+		await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+	}
+}
+const test = async () => {
+	const repo = new UserRepository('users.json');
+	await repo.create({ email: 'a@gmail.com', password: 'dffs' });
+	const users = await repo.getall();
+	console.log(users);
+};
+
+test();
