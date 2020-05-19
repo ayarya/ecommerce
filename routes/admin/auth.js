29390db56758803a78976111541077caeb55c188
Ayush Arya <arya.ayush409@gmail.com>
2020-05-19 02:43:52 -0400

    check validation and sanitization added

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users.js');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -9,19 +10,34 @@ router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
-	const exisitingUser = await usersRepo.getOneBy({ email: email });
-	if (exisitingUser) {
-		return res.send('Email in Use');
+router.post(
+	'/signup',
+	[
+		check('email').trim().normalizeEmail().isEmail().custom(async (email) => {
+			const exisitingUser = await usersRepo.getOneBy({ email });
+			if (exisitingUser) {
+				throw new Error('Email in Use');
+			}
+		}),
+		check('password').trim().isLength({ min: 4, max: 20 }),
+		check('passwordConfirmation')
+			.trim()
+			.isLength({ min: 4, max: 20 })
+			.custom(async (passwordConfirmation, { req }) => {
+				if (passwordConfirmation !== req.body.password) {
+					throw new Error('Password must match');
+				}
+			})
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
+		const { email, password, passwordConfirmation } = req.body;
+		const user = await usersRepo.create({ email, password }); // email : email, es6>
+		req.session.userId = user.id;
+		res.send('Done');
 	}
-	if (password !== passwordConfirmation) {
-		return res.send('Password must match');
-	}
-	const user = await usersRepo.create({ email, password }); // email : email, es6>
-	req.session.userId = user.id;
-	res.send('Done');
-});
+);
 
 router.get('/signout', (req, res) => {
 	req.session = null;
