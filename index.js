e62a23f88e9d56c4b33fff3b79584468ce6daf2a
Ayush Arya <arya.ayush409@gmail.com>
2020-05-19 00:52:44 -0400

    sigin route added

--- index.js
+++ index.js
@@ -51,7 +51,17 @@ app.get('/signin', (req, res) => {
     `);
 });
 
-app.post('/signin', async (req, res) => {});
+app.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+	const user = await usersRepo.getOneBy({ email });
+	if (!user) {
+		return res.send('Email not found');
+	}
+	if (user.password !== password) {
+		return res.send('password not correct');
+	}
+	req.session.userId = user.id;
+});
 app.listen(3000, () => {
 	console.log('listening ');
 });
